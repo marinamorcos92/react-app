82e45543830aa46973089c909acdbffba3d39cbc
Marina Morcos <marinamorcos@hotmail.com>
2023-04-18 12:49:29 +0200

    import axios and the weather app from sandbox to VS code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Weather from "./Weather.js";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Weather App React </h1>
+    <Weather />
   </React.StrictMode>
 );
 
